710e27106cd3116e56d06701c618cb769fc58ac4
KauanRedBlack <114826891+KauanRedBlack@users.noreply.github.com>
2024-04-09 22:35:37 -0300

    não funcional a parte de destaque do selecionamento da task

--- script-crud.js
+++ script-crud.js
@@ -1,7 +1,10 @@
 const btnaddtask = document.querySelector('.app__button--add-task')
 const formaddtask = document.querySelector('.app__form-add-task')
 const textarea = document.querySelector('.app__form-textarea')
+const descriptiontask = document.querySelector('.app__section-active-task-description')
 const ultarefas = document.querySelector('.app__section-task-list')
+const btncanceltask = document.querySelector('.app__form-footer__button--cancel')
+const btndeletedtask = document.querySelector('.app__form-footer__button--delete')
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 
 function criarElementoTarefa(tarefa) {
@@ -10,21 +13,29 @@ function criarElementoTarefa(tarefa) {
 
     const svg = document.createElement('svg')
     svg.innerHTML = `
-    <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none"
-    xmlns="http://www.w3.org/2000/svg">
-    <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
-    <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
-        fill="#01080E"></path>
-</svg>
-
+        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
+                fill="#01080E"></path>
+        </svg>
     `
     const paragrafo = document.createElement('p')
     paragrafo.textContent = tarefa.descricao
     paragrafo.classList.add('app__section-task-list-item-description')
 
-
     const botao = document.createElement('button')
     botao.classList.add('app_button-edit')
+    botao.onclick = () => {
+        const novatarefa = prompt("qual é o nome da nova tarefa?")
+        if (novatarefa){
+            paragrafo.textContent = novatarefa
+            tarefa.descricao = novatarefa
+            upgradetask()
+            return
+        }
+        
+    }
 
     const imagemBotao = document.createElement('img')
     imagemBotao.setAttribute('src', '/imagens/icons/edit.png')
@@ -33,7 +44,16 @@ function criarElementoTarefa(tarefa) {
     li.append(svg)
     li.append(paragrafo)
     li.append(botao)
+
+    li.onclick = () => {
+        descriptiontask.textContent = tarefa.descricao
+        li.classList.add('.app__section-task-list-item-active')
+    }
     return li
+}   
+
+function upgradetask () {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
 }
 
 btnaddtask.addEventListener('click', () => {
@@ -43,13 +63,26 @@ btnaddtask.addEventListener('click', () => {
 formaddtask.addEventListener('submit', (event) => {
     event.preventDefault();
     const tarefa = {
-        descrição: textarea.value
+        descricao: textarea.value
     }
-    tarefas.push(tarefa)               //aqui funciona como um append para adicionar dentro da lista
-    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+    tarefas.push(tarefa)
+    const elementoTarefa = criarElementoTarefa(tarefa)
+    ultarefas.append(elementoTarefa)
+    upgradetask()
+    textarea.value = ''
+    formaddtask.classList.add('hidden')
 })
 
 tarefas.forEach(tarefa => {
     const elementotarefa = criarElementoTarefa(tarefa)
     ultarefas.append(elementotarefa)
 });
+
+btncanceltask.addEventListener('click', () => {
+    formaddtask.classList.toggle('hidden');
+})
+
+btndeletedtask.addEventListener('click', () => {
+    textarea.value = ''
+    formaddtask.classList.add('hidden')
+})
